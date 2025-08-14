462fb19404b73527c214f13efbaef3d47ea3ab55
Mael Bcd <mael.boccardi@gmail.com>
2025-08-14 12:52:38 +0200

    Fixing regimes

--- app.py
+++ app.py
@@ -625,9 +625,8 @@ def run_full_pipeline(n_clicks):
         ],
     }
     grouped_contribs = aggregate_contributions_by_group(variable_contribs, group_map)
-    # regimes = classify_adaptive_regime_hybrid_fallback(fsi_series, quantile_window=1260)
-    # regimes = classify_risk_regime_hybrid(fsi_series)
-    regimes_full = classify_risk_regime_hybrid(variable_contribs['FSI'])
+    regimes_full = classify_adaptive_regime_hybrid_fallback(variable_contribs['FSI'], quantile_window=1260)
+    # regimes_full = classify_risk_regime_hybrid(variable_contribs['FSI'])
 
     df_aligned = df.loc[fsi_series.index].copy()
     df_aligned["Regime"] = regimes_full.astype(str)
